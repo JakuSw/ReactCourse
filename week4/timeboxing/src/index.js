67b57deb4b03f59930c6a530c1f34f026d9cda76
Jakub Sw <nja.mt2@gmail.com>
2020-09-04 12:47:43 +0200

    Sass added

--- week4/timeboxing/src/index.js
+++ week4/timeboxing/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./components/App"
+import App from "./components/App";
+import "./styles/main.scss";
 
 const rootElement = document.getElementById("root");
 const element = (<div> 
