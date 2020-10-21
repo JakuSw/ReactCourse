c9d0d0913e9430e17ac8e056f5a98ebed0b72301
Jakub Sw <nja.mt2@gmail.com>
2020-10-21 23:12:27 +0200

    TimeboxList with useEffect

--- project/src/components/TimeboxList.js
+++ project/src/components/TimeboxList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect, useContext} from "react";
 
 import TimeboxCreator from "./TimeboxCreator";
 import ErrorBoundary from "./ErrorBoundary";
@@ -8,88 +8,85 @@ import AuthenticationContext from "../contexts/AuthenticationContext";
 const Timebox = React.lazy(() => import("./Timebox"));
 
 
-class TimeboxList extends React.Component{
-    state = {
-        timeboxes: [],
-        loading:true,
-        error:null
-    }
+function TimeboxList() {
+    const [timeboxes, setTimeboxes] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const { accessToken } = useContext(AuthenticationContext)
 
-    componentDidMount() {
-        TimeboxesAPI.getAllTimeboxes(this.context.accessToken).then(
-            (timeboxes) => this.setState({ timeboxes })
+    useEffect(() => {
+        TimeboxesAPI.getAllTimeboxes(accessToken).then(
+            (timeboxes) => setTimeboxes(timeboxes)
         ).catch(
-            (error) => this.setState({ error })
+            (error) => setError(error)
         ).finally(
-            () => this.setState({loading: false})
+            () => setLoading(false)
         )
-    }
+    })
 
-
-    addTimebox = (timebox) => {
-        TimeboxesAPI.addTimebox(timebox, this.context.accessToken).then(
-            (addedTimebox) => this.setState(prevState => {
-                const timeboxes = [...prevState.timeboxes, addedTimebox];
-                return {timeboxes};
+    function addTimebox(timebox)  {
+        TimeboxesAPI.addTimebox(timebox, accessToken).then(
+            (addedTimebox) => setTimeboxes(prevState => {
+                const timeboxes = [...prevState, addedTimebox];
+                return timeboxes;
             })
         )
         
     }
 
-    removeTimebox = (indexToRemove) => {
-        TimeboxesAPI.removeTimebox(this.state.timeboxes[indexToRemove],this.context.accessToken)
+    function removeTimebox(indexToRemove) {
+        TimeboxesAPI.removeTimebox(timeboxes[indexToRemove],accessToken)
             .then(
-                () => this.setState(prevState => {
-                    const timeboxes = prevState.timeboxes.filter((timebox, index) => index !== indexToRemove);
-                    return {timeboxes};
+                () => setTimeboxes(prevState => {
+                    const timeboxes = prevState.filter((timebox, index) => index !== indexToRemove);
+                    return timeboxes;
                 })
             )   
     }
 
-    updateTimebox = (indexToUpdate, timeboxToUpdate) => {
-        TimeboxesAPI.replaceTimebox(timeboxToUpdate, this.context.accessToken).then(
-            (updatedTimebox) => this.setState(prevState => {
-                const timeboxes = prevState.timeboxes.map((timebox, index) => 
+    function updateTimebox(indexToUpdate, timeboxToUpdate) {
+        TimeboxesAPI.replaceTimebox(timeboxToUpdate, accessToken).then(
+            (updatedTimebox) => setTimeboxes(prevState => {
+                const timeboxes = prevState.map((timebox, index) => 
                     index === indexToUpdate ? updatedTimebox : timebox
                 );
-                return {timeboxes};
+                return timeboxes;
             })
         )
         
     }
 
-    handleCreate = (createdTimebox) => {
+    function handleCreate(createdTimebox) {
         try {
-            this.addTimebox(createdTimebox)
+            addTimebox(createdTimebox)
         } catch(error) {
             console.log("Error while creating TImebox", error);
         }
     }
 
-    render() {
-        return (
-            <>
-                <TimeboxCreator onCreate={this.handleCreate}/>
-                <ErrorBoundary message="Some error in timebox list">
-                {this.state.loading ? "Loading" : null}
-                {this.state.error ? "Something went wrong" : null}
-                {
-                    this.state.timeboxes.map((timebox, index) => (
-                        <React.Suspense fallback="...Loading">
-                            <Timebox 
-                                key={timebox.id} 
-                                title={timebox.title} 
-                                totalTimeInMinutes={timebox.totalTimeInMinutes}
-                                onDelete={() => this.removeTimebox(index)}
-                                onEdit={(updatedTitle) => this.updateTimebox(index, {...timebox, title: `${updatedTitle}`})}
-                                />
-                        </React.Suspense>
-                ))}
-                </ErrorBoundary>
-            </>
-        )
-    }
+    return (
+        <>
+
+            <TimeboxCreator onCreate={handleCreate}/>
+            <ErrorBoundary message="Some error in timebox list">
+            {loading ? "Loading" : null}
+            {error ? "Something went wrong" : null}
+            {
+                timeboxes.map((timebox, index) => (
+                    <React.Suspense fallback="...Loading">
+                        <Timebox 
+                            key={timebox.id} 
+                            title={timebox.title} 
+                            totalTimeInMinutes={timebox.totalTimeInMinutes}
+                            onDelete={() => removeTimebox(index)}
+                            onEdit={(updatedTitle) => updateTimebox(index, {...timebox, title: `${updatedTitle}`})}
+                            />
+                    </React.Suspense>
+            ))}
+            </ErrorBoundary>
+            
+        </>
+    )
 }
-TimeboxList.contextType = AuthenticationContext;
 
 export default TimeboxList;
